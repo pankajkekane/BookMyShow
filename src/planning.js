37dff48a58200f4a5fe9de0c48e893185c6b3afb
pankajkekane <kekanepankaj@gmail.com>
2021-07-16 20:19:06 +0530

    setting all things

---
+++ src/planning.js
@@ -0,0 +1,23 @@
+//book my show planning
+
+// -> we need a reusable corousel
+//  -> hero corousel 
+//  -> common corousel
+//  -> cast and crew small carousel
+
+
+// pages
+// -> ho,e page 
+// -> movie page 
+// -> categories page 
+// -> auth page 
+// -> razorpay payment gateway
+
+//plan -> how are we building it ????
+
+
+//HOC -> higher order components
+
+// Layout first approach ->  lets immagine non changable content will not change so it will not take time to refresh it. but we will only change changable content on  every page and update it in non changable content.
+
+// how to change the comtent in navbar -> we will pass the props based on condition to changes ( for short changes)  or  we will build different navbar for diffrent pages ( for lots of changes)
