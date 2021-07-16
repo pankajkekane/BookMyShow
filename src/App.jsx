fcd8be5ae125083fa81a72f70c68cf6e89d6a075
pankajkekane <kekanepankaj@gmail.com>
2021-07-16 19:20:46 +0530

    starting project

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 
 function App() {
   return(
-  <div className="App"></div>
+  <div className="App">
+    <h1 className="text-8xl	hover:text-blue-500">hello react</h1>
+
+    <button className="border-2 border-blue-500 px-2 py-1 rounded-md bg-blue-500 text-white hover:bg-white hover:border-2 hover:text-blue">hello tailwind</button>
+  </div>
   )
   
 }
