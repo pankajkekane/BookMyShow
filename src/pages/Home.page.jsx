9305e5344fb0ec4ad40b4b89ae3f921d862f53d7
pankajkekane <kekanepankaj@gmail.com>
2021-08-13 16:16:51 +0530

    v 33

--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -10,9 +10,20 @@ const HomePage = () => {
     return(
         <>
          <div className="container mx-auto px-28">
-             <h1 className="text-xl font-bold text-grey-800  my-3">The Best of Entertainment</h1>
+             <h1 className="text-xl font-bold text-grey-800 py-3">The Best of Entertainment</h1>
         <EntertainmentCardSlider/>
+        </div>
+
+        <div className= "bg-bms-800 py-16">
+            <div className="hidden md:flex">
+                <div>
+                    <img  src="https://in.bmscdn.com/discovery-catalog/collections/tr:w-1440,h-120/premiere-rupay-banner-web-collection-202104230555.png" alt="Premier" />
+                </div>
+               
+            </div>
+        <div className="container mx-auto px-20">
         <Premier/>
+        </div>
         </div>
         </>
     );
