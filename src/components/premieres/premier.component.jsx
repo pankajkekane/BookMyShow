5c99e7e89ccd3c4e75107088e17c5cb7634afe7a
pankajkekane <kekanepankaj@gmail.com>
2021-08-13 15:18:13 +0530

    premier

--- src/components/premieres/premier.component.jsx
+++ src/components/premieres/premier.component.jsx
@@ -6,7 +6,8 @@ import Slider from 'react-slick';
 import Poster from '../poster/poster.component';
 
 
-export const premier = () => {
+export const Premier = () => {
+
 
     const settings = {
         Infinity:false,
@@ -46,11 +47,78 @@ export const premier = () => {
     };
 
 
+const PremierImages = [
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00122511-myxaqdugrd-portrait.jpg",
+        alt: "Godzilla",
+        title: "Godzilla vs. Kong",
+        subtitle: "English",
+    },
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00304964-sppgefaccr-portrait.jpg",
+        alt: "Rewind",
+        title: "Rewind",
+        subtitle: "Kannada",
+    },
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00047164-appgcvtkry-portrait.jpg",
+        alt: "Batman",
+        title: "Zack Snyder's Justice Legue",
+        subtitle: "English",
+    },
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00138770-kfmhzakdwj-portrait.jpg",
+        alt: "Tailgate",
+        title: "Tailgate",
+        subtitle: "English",
+    },
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00301282-dnzbnayacy-portrait.jpg",
+        alt: "Kabullywood",
+        title: "Kabullywood",
+        subtitle: "Farsi",
+    },
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00302306-xxwtjnexzz-portrait.jpg",
+        alt: "Proximity",
+        title: "Proximity",
+        subtitle: "English",
+    },
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00137312-evsmpzefqs-portrait.jpg",
+        alt: "The Secrets We Keep",
+        title: "The Secrets We Keep",
+        subtitle: "English",
+    },
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00308446-jldllpekyw-portrait.jpg",
+        alt: "Mortal Kombat",
+        title: "Mortal Kombat",
+        subtitle: "English",
+    },
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00310901-lvctbfwtyb-portrait.jpg",
+        alt: "Ammonite",
+        title: "Ammonite",
+        subtitle: "English",
+    },
+    {
+        src:"https://in.bmscdn.com/discovery-catalog/events/tr:w-400,h-600,bg-CCCCCC:oi-discovery-catalog@@icons@@premiere-icon.png,ox-322,oy-20/et00077622-ltlsaebkxh-portrait.jpg",
+        alt: "Wonder Women 1984",
+        title: "Wonder Women 1984",
+        subtitle: "English",
+    }
+]
+
     return (
         <>
-           <Slider {...settings}></Slider>
+           <Slider {...settings}>
+               {PremierImages.map((image) =>(
+                   <Poster {...image}/>
+               ))}
+           </Slider>
         </>
     )
 }
 
-export default premier;
+export default Premier;
--- src/pages/Home.page.jsx
+++ src/pages/Home.page.jsx
@@ -2,6 +2,8 @@ import React from "react";
 
 
 import EntertainmentCardSlider from "../components/Entertainment/Entertainmentcard.component";
+import Premier from "../components/premieres/premier.component";
+
 
 
 const HomePage = () => {
@@ -10,6 +12,7 @@ const HomePage = () => {
          <div className="container mx-auto px-28">
              <h1 className="text-xl font-bold text-grey-800  my-3">The Best of Entertainment</h1>
         <EntertainmentCardSlider/>
+        <Premier/>
         </div>
         </>
     );
